1220710f26b97d13a387e0873565759351303f56
Philip Ritzer <philip.ritzer@dlr.de>
2023-05-17 13:58:25 +0200

    valid file path

--- experiments/ql_2way-single-intersection.py
+++ experiments/ql_2way-single-intersection.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     )
     prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
     args = prs.parse_args()
-    experiment_time = str(datetime.now()).split(".")[0]
+    experiment_time = datetime.now().strftime("%y%m%d_%H%M%S")
     out_csv = f"outputs/2way-single-intersection/{experiment_time}_alpha{args.alpha}_gamma{args.gamma}_eps{args.epsilon}_decay{args.decay}_reward{args.reward}"
 
     env = SumoEnvironment(
